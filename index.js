f81273f22a85aa613d25a192b8139145026565da
gautaz <11046232+gautaz@users.noreply.github.com>
2022-03-14 17:51:34 +0100

    fix rules overwrite by unicorn

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ module.exports = {
   rules:
   // tag::rules[]
   {
+    // eslint-plugin-unicorn will only produce warnings for now
+    ...Object.fromEntries(Object.entries(unicorn.configs.recommended.rules).map(
+      ([rule, setting]) => setting === 'error' ? [rule, 'warn'] : [rule, setting]
+    )),
     // https://prettier.io/docs/en/integrating-with-linters.html#use-eslint-to-run-prettier
     // https://github.com/prettier/eslint-plugin-prettier#options
     'prettier/prettier': [
@@ -61,18 +65,12 @@ module.exports = {
     'max-depth': ['error', { max: 2 }],
     // https://eslint.org/docs/rules/max-params
     'max-params': ['error', {max: 3}],
-    // https://eslint.org/docs/rules/no-nested-ternary
-    'no-nested-ternary': 'error',
     // https://eslint.org/docs/rules/no-trailing-spaces
     'no-trailing-spaces': 'error',
     // https://eslint.org/docs/rules/one-var
     'one-var': ['error', 'never'],
     // https://github.com/avajs/eslint-plugin-ava/blob/main/docs/rules/prefer-power-assert.md
     'ava/prefer-power-assert': 'warn',
-    // eslint-plugin-unicorn will only produce warnings for now
-    ...Object.fromEntries(Object.entries(unicorn.configs.recommended.rules).map(
-      ([rule, setting]) => setting === 'error' ? [rule, 'warn'] : [rule, setting]
-    ))
   },
   // end::rules[]
 };
